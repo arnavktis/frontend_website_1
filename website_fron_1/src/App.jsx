050c3236e81d04191e5a5c240830e5b6ab5868f4
Arnav khanna <arnavktis@gmail.com>
2024-03-14 01:22:10 +0530

    hero and navbar created

---
+++ website_fron_1/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Header from "./components/header";
+import HeroSection from "./components/heroSection";
+import './assets/css/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/vendor/bootstrap/css/bootstrap-grid.min.css';
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
+import './assets/vendor/fontawesome-free/css/all.min.css';
+import './assets/vendor/swiper/swiper-bundle.min.css';
+
+
+// import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <head>
+          {/* Favicons */}
+          <link href="assets/img/favicon.png" rel="icon" />
+          <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+
+          {/* Google font */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
+        </head>
+        <Header></Header>
+        <HeroSection></HeroSection>
+      </body>
+    </div>
+  );
+}
+
+export default App;
