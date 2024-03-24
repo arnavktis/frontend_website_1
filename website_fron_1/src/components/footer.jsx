f227079687023e8dca225e75be6b25ad3503f315
Arnav khanna <arnavktis@gmail.com>
2024-03-24 11:43:31 +0530

    footer changes

--- website_fron_1/src/components/footer.jsx
+++ website_fron_1/src/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
                 <div className="row">
                     <div className="col-lg-4 col-md-6">
                     <div className="footer-info">
-                        <h3>UpConstruction</h3>
+                        <h3>Jn Engineer</h3>
                         <p>
                         A108 Adam Street <br />
                         NY 535022, USA
@@ -76,14 +76,10 @@ function Footer() {
             <div className="footer-legal text-center position-relative">
                 <div className="container">
                 <div className="copyright">
-                    &copy; Copyright <strong><span>UpConstruction</span></strong>. All Rights Reserved
+                    &copy; Copyright <strong><span>JNEngineers</span></strong>. All Rights Reserved
                 </div>
                 <div className="credits">
-                    {/* All the links in the footer should remain intact. */}
-                    {/* You can delete the links only if you purchased the pro version. */}
-                    {/* Licensing information: https://bootstrapmade.com/license/ */}
-                    {/* Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/ */}
-                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                    Designed by <a href="">Jn Engineers</a>
                 </div>
                 </div>
             </div>
