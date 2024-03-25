d6eb35a5f03b59531ed063449dd5c8ff89bfb7b6
Avneesh-Sinha <avneesh0904@gmail.com>
2024-03-25 11:49:05 +0530

    .

--- website_fron_1/src/components/header.jsx
+++ website_fron_1/src/components/header.jsx
@@ -7,6 +7,10 @@ function Header() {
   // Capitalized the function name to follow convention
   const [menuOpen, setMenuOpen] = useState(false);
   const [crossOpen, setCrossOpen] = useState(false);
+  const [tabNum, setTabNum] = useState(1);
+    const getTabNum = (option) => {
+        setTabNum(option);
+    };
   return (
     <div className="header">
       <header id="header" className="header d-flex align-items-center">
@@ -31,22 +35,29 @@ function Header() {
               {crossOpen && (<li className="cross" onClick={() =>{setMenuOpen(false); setCrossOpen(false);}}>
                 <a>X</a>
               </li>)}
-              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false);}}>
-                <Link to="/" className="active">
+              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false); getTabNum(1);}}>
+                <Link to="/" className={tabNum===1 ? "active": ""}>
                   Home
                 </Link>
               </li>
-              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false);}}>
-                <Link to="/about">About</Link>
+              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false); getTabNum(2);}}>
+                <Link to="/about" className={tabNum===2 ? "active": ""}>
+                  About
+                </Link>
               </li>
-              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false);}}>
-                <Link to="/equipment">Equipment</Link>
+              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false); getTabNum(3);}}>
+                <Link to="/equipment" className={tabNum===3 ? "active": ""}>
+                  Equipment
+                </Link>
               </li>
-              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false);}}>
-                <Link to="/product">Products</Link>
+              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false); getTabNum(4);}}>
+                <Link to="/product" className={tabNum===4 ? "active": ""}>
+                  Products
+                </Link>
               </li>
-              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false);}}>
-                <a href="/contact">Contact</a>
+              <li onClick={() =>{setMenuOpen(false); setCrossOpen(false); getTabNum(5);}}>
+                <a href="/contact" className={tabNum===5 ? "active": ""}>
+                  Contact</a>
               </li>{" "}
             </ul>
           </nav>
